dce7ff39d5e18539e9b4ceebf256d6dd17079dc3
Ramon Valeriano <rrvaleriano@gmail.com>
2026-01-27 12:21:49 -0300

    feat(): Ajustando codigo de cors

--- backend/main.py
+++ backend/main.py
@@ -19,8 +19,8 @@ app = FastAPI(title=settings.project_name, lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
